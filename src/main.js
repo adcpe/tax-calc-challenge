468aa075f56c72227687424c78d1333551e38aaa
Andrés Del Carpio <andresdc@andresdc.net>
2022-11-01 12:02:50 -0500

    Main app finished

---
+++ src/main.js
@@ -0,0 +1,65 @@
+function calculateLineTax(subTotal, isImported, isExempt) {
+  let lineTax = isImported ? subTotal * 0.05 : 0
+  lineTax = isExempt ? lineTax : lineTax + subTotal * 0.1
+  return lineTax
+}
+
+function convertLines(lines) {
+  return lines.map((line) => {
+    const fields = line.split(' ')
+    const noTax = new RegExp(/pill|chocolate|book/)
+
+    const quantity = fields[0]
+    const description = fields.slice(1, -2).join(' ')
+    const unitPrice = +fields.slice(-1).join()
+    const subTotal = quantity * unitPrice
+
+    const isImported = description.indexOf('import') > -1
+    const isExempt = Array.isArray(description.match(noTax))
+    const tax = calculateLineTax(subTotal, isImported, isExempt)
+
+    return {
+      quantity,
+      description,
+      unitPrice,
+      subTotal,
+      tax
+    }
+  })
+}
+
+function calculateSalesTaxTotal(lines) {
+  let taxes = lines.map((line) => line.tax)
+  taxes = taxes.reduce((prevValue, currValue) => prevValue + currValue, 0)
+  return parseFloat(taxes).toFixed(2)
+}
+
+function printOutput(lines) {
+  const output = document.querySelector('#output')
+  const salesTaxVal = calculateSalesTaxTotal(lines)
+  let totalVal = 0
+
+  lines.forEach((line) => {
+    const lineOutput = document.createElement('p')
+    totalVal += line.subTotal + line.tax
+    lineOutput.innerText = `${line.quantity} ${line.description}: ${line.subTotal}`
+    output.appendChild(lineOutput)
+  })
+
+  const salesTax = document.createElement('p')
+  salesTax.innerText = `Sales Taxes: ${salesTaxVal}`
+  output.appendChild(salesTax)
+
+  const total = document.createElement('p')
+  total.innerText = `Total: ${totalVal.toFixed(2)}`
+  output.appendChild(total)
+}
+
+function process() {
+  let lines = document.querySelector('#input').value.split('\n')
+  lines = convertLines(lines)
+  printOutput(lines)
+}
+
+const calculateButton = document.querySelector('#calculate')
+calculateButton.addEventListener('click', process)
